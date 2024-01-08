3505a2ce84429ac56cb77ad828fe705c1f5f03d4
KievL <kiev.sider@hotmail.com>
2024-01-08 18:38:07 -0300

    fix4

--- Soothsayer/settings.py
+++ Soothsayer/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-imfgd2bdf##&wyr-#t+@fp91h#ed^g@(slyl$0xlhheq$a8wba'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
